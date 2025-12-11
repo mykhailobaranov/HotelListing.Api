9702fa99d8bef048214d611b105678e8df07cfac
mykhailobaranov <baranovhw@gmail.com>
2025-12-11 16:21:28 +0200

    Configure database context and connection string using EF Core

---
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : base(options) { }
+    }
+}
