7005f50d858ea044187c66a588ebee867f7e2259
mykhailobaranov <baranovhw@gmail.com>
2026-02-05 20:57:11 +0200

    Setup Identity and add initial Identity migration

--- HotelListing.Api/Data/HotelListingDbContext.cs
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using HotelListing.Api.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Api.Data
 {
-    public class HotelListingDbContext : DbContext
+    public class HotelListingDbContext : IdentityDbContext<ApplicationUser>
     {
         public HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : base(options) { }
 
--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -1,8 +1,10 @@
 using HotelListing.Api.Data;
 using HotelListing.Api.MappingProfiles;
+using HotelListing.Api.Models.Domain;
 using HotelListing.Api.Repositories.Implementation;
 using HotelListing.Api.Repositories.Interface;
 using HotelListing.Api.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,8 +17,11 @@ builder.Services.AddControllers().AddJsonOptions(opt=>
     opt.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddIdentityCore<ApplicationUser>(options => { })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<HotelListingDbContext>();
 
+builder.Services.AddOpenApi();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 builder.Services.AddScoped<IHotelsRepository, HotelsRepository>();
